e9502f338c84939a1fd16e372c6c1393648a6e17
Hitesh Patil <hiteshpatil0205@gmail.com>
2025-07-09 22:43:02 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-amber-400'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero recusandae distinctio necessitatibus voluptates repellendus, corporis quo ipsa omnis veniam. Doloremque aut beatae perspiciatis voluptates natus in aperiam reprehenderit, optio maiores odio est quos tenetur. Architecto, officia inventore? Facilis, in deserunt nemo illum blanditiis officia accusamus.
+    </div>
+  )
+}
+
+export default App
